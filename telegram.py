977639d036822dbd118412c4a7d99d82e0c8764a
JuGeo <nahum27@naver.com>
2018-10-29 06:25:07 +0000

    telegram

---
+++ telegram.py
@@ -0,0 +1,57 @@
+
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+token = '637784833:AAEfmbDKEl1W-_fulbqHBMpcTKscfatbhSk'
+method_name = 'getUpdates'
+url = 'https://api.telegram.org/bot{}/{}'.format(token, method_name)
+
+update = requests.get(url).json()
+#print(update)
+#print(update['result'][0]['message']['from']['id'])
+
+user_id = update['result'][0]['message']['from']['id'] #'762656647'
+
+method_name = 'sendmessage'
+msg = '안녕하세요'
+
+
+url = 'https://finance.naver.com/sise/'
+req = requests.get(url).text
+soup = bs(req,'html.parser')    #html.parser
+#print(soup)
+
+select = soup.select_one('#KOSPI_now').text
+
+msg_url = 'https://api.telegram.org/bot{}/{}?chat_id={}&text={}'.format(token, method_name, user_id, select)
+
+
+# import requests
+
+# url = 'https://finance.naver.com/sise/'
+# req = requests.get(url).text
+# soup = bs(req,'lxml')    #html.parser
+# #print(soup)
+
+# select = soup.select_one('#KOSPI_now')
+
+#print(type(select))
+# print(select.text)
+
+
+
+# print(msg_url)
+print(requests.get(msg_url))
+
+
+
+
+
+
+#print(url)
+#result = requests.get(url).text
+
+#print(result)
+
+
+#print(json.load(result, encoding = 'utf-8'))
